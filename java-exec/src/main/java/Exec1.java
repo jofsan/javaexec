183b4a7bd4d6901f20fea7629b847b4f73077486
jofsan <jonatas.santos@sptech.school>
2023-02-12 20:19:14 -0300

    First commit

---
+++ java-exec/src/main/java/Exec1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class Exec1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Diga uma frase: ");
+        String frase = leitor.nextLine();
+        
+        System.out.println("A frase digitada foi: "+frase+ " :-)");
+    }
+}
