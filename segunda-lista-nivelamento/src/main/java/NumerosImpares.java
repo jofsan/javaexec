0323160c90d2f048484c05b43d9dc34f8765cc57
jofsan <jonatas.santos@sptech.school>
2023-02-16 02:00:11 -0300

    NumeroImpares

---
+++ segunda-lista-nivelamento/src/main/java/NumerosImpares.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class NumerosImpares {
+    public static void main(String[] args) {
+        for(int i = 0;i<=90;i++){
+            if(i%2==0){
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
