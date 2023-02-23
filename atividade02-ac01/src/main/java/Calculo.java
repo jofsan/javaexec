08090bb93a8707a02a3af249d897843b653c24ca
jofsan <jonatas.santos@sptech.school>
2023-02-23 00:39:40 -0300

    return JAVA

---
+++ atividade02-ac01/src/main/java/Calculo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class Calculo {
+    Double calcularMedia(Double nota1, Double nota2){
+        return ((nota1*0.4)+(nota2*0.6));
+    }
+}
