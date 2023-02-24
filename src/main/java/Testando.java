4dc64b871209ae42f3c0eb809505b708b1ec2bcb
jofsa <jofsa@192.168.1.10>
2023-02-24 01:01:53 -0300

    teste

---
+++ src/main/java/Testando.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofsa
+ */
+public class Testando {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        System.out.println("teste");
+        String teste = leitor.nextLine();
+    }
+}
