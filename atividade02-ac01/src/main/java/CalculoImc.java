5a11775ccf9527b95ee30656c7b1da2f4c3588a2
jofsan <jonatas.santos@sptech.school>
2023-02-24 21:07:22 -0300

    métodos JAVA

---
+++ atividade02-ac01/src/main/java/CalculoImc.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class CalculoImc {
+    Double calculaIMC( Double peso, Double altura){
+        return peso/(altura*altura);
+    }
+}
