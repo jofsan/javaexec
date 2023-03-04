5467130e1f44ee464f9ff8ca2658d8a690361b8e
jofsan <jonatas.santos@sptech.school>
2023-03-04 20:00:37 -0300

    treino AC1

---
+++ treino-prova/src/main/java/Treino1.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class Treino1 {
+    public static void main(String[] args) {
+         Scanner leitor = new Scanner(System.in);
+         
+         System.out.println("Digite um número entre 0 e 10: ");
+         Integer numeroUser = leitor.nextInt();
+         Integer numeroSorteado = ThreadLocalRandom.current().nextInt(0, 11);
+         
+         System.out.println("numero sorteado: \n"+numeroSorteado);
+         int cont =0;
+         while(numeroSorteado!=numeroUser){
+             cont++;
+             System.out.println(numeroSorteado = ThreadLocalRandom.current().nextInt(0, 11));
+            
+           
+         }
+         System.out.println("Acertou!");
+           if(cont<=3){
+                 System.out.println("Você é muito sortudo!");
+             }else if (cont>=4&&cont<=10){
+                 System.out.println("você é sortudo");
+             }else{
+                 System.out.println("É melhor você parar de apostar e ir trabalhar!");
+             }
+    }
+   
+    
+}
