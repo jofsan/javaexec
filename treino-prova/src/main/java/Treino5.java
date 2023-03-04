afff0578ed1270eacef90f7ed82595b3cc65d8f8
jofsan <jonatas.santos@sptech.school>
2023-03-04 20:37:45 -0300

    treino5 AC1

---
+++ treino-prova/src/main/java/Treino5.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class Treino5 {
+    public static void main(String[] args) {
+        for(int i= 0;i<=90;i++){
+            if(i%2==1){
+                System.out.println(i);
+            }
+        }
+    }
+}
