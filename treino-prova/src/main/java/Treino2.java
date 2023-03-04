e3400575cf2d70df4f82ca4092201d6954189c6a
jofsan <jonatas.santos@sptech.school>
2023-03-04 20:08:04 -0300

    treino2 AC1

---
+++ treino-prova/src/main/java/Treino2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class Treino2 {
+    
+    public static void main(String[] args) {
+        Integer soma = 0;
+       for(int x=10;x>0;x--){
+           soma+=x;
+           
+       }
+       System.out.println("A soma dos numeros é: "+soma);
+    }
+}
