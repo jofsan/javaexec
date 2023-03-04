7baadf80cf299ca135723e2cf1f55692eaf7e800
jofsan <jonatas.santos@sptech.school>
2023-03-04 20:36:11 -0300

    treino4 AC1

---
+++ treino-prova/src/main/java/Treino4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jofss
+ */
+public class Treino4 {
+    public static void main(String[] args) {
+        int cont = 0;
+        while(cont<=40){
+            cont++;
+       if(cont%2==0){
+           System.out.println(cont);
+       }
+        }
+    }
+}
